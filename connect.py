2973bcf0a959189b22d47af14b54ee1516a5dedb
Ezequiel Klug <56704174+ezeklug@users.noreply.github.com>
2021-12-09 17:47:43 -0300

    Add files via upload

---
+++ connect.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+import psycopg2
+from config import config
+from PIL import Image
+import json
+def connect():
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # read connection parameters
+        params = config()
+
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+
+        conn = psycopg2.connect(**params)
+        # create a cursor
+        cur = conn.cursor()
+        
+        print('Reading json file')
+        query = f"INSERT INTO public.imagenes(path, vector, id_hoja) VALUES"
+        with open('data.txt') as json_file:
+            data = json.load(json_file)
+            for p in data['imagenes']:
+                path = p['path']
+                vector = p['vector']
+                query+=f"('{path}', ARRAY{vector}, null),"
+            query = query[:-1] + ';'
+            print('json file read successfully')
+        print('Executing Insert query')
+        cur.execute(query)
+        print('Commiting')
+        conn.commit()
+
+        
+	# close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
+
+if __name__ == '__main__':
+    connect()
