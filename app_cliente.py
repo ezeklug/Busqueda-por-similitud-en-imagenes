0e8ce4d190c7e5ebc45b894887be61763b0d473d
Ezequiel Klug <56704174+ezeklug@users.noreply.github.com>
2021-12-10 19:33:32 -0300

    Add files via upload

--- app_cliente.py
+++ app_cliente.py
@@ -5,6 +5,9 @@ from PIL import Image
 from img2vec_pytorch import Img2Vec
 from psycopg2.extras import RealDictCursor
 from pathlib import Path
+import urllib.request as ur
+import os, ssl
+import webbrowser
 
 
 def insert(value):
@@ -52,7 +55,11 @@ def place(elem):
     '''
     return sg.Column([[elem]], pad=(0,0))
 
-def main():
+def main():  
+    if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
+    getattr(ssl, '_create_unverified_context', None)):
+        ssl._create_default_https_context = ssl._create_unverified_context
+    
     img2vec = Img2Vec(cuda=False)
     sg.theme("DarkBlue3")
     sg.set_options(font=("Microsoft JhengHei", 16))
@@ -90,80 +97,80 @@ def main():
             place(sg.Text(key='vecino1_id_hoja', visible = False)),
             place(sg.Text(key='vecino1_distancia', visible = False)),
             place(sg.Text(key='vecino1_path', visible = False)),
-            place(sg.Input(key='vecino1_web_path', visible = False)),
-            place(sg.Button('Open', key='Open1', visible = False))
+            place(sg.Input(key='web_path_vecino1', visible = False)),
+            place(sg.Button('Open', key='Open 1', visible = False))
         ],
         [
             place(sg.Text(key='vecino2_id', visible = False)),
             place(sg.Text(key='vecino2_id_hoja', visible = False)),
             place(sg.Text(key='vecino2_distancia', visible = False)),
             place(sg.Text(key='vecino2_path', visible = False)),
-            place(sg.Input(key='vecino2_web_path', visible = False)),
-            place(sg.Button('Open', key='Open2', visible = False))
+            place(sg.Input(key='web_path_vecino2', visible = False)),
+            place(sg.Button('Open', key='Open 2', visible = False))
         ],
         [
             place(sg.Text(key='vecino3_id', visible = False)),
             place(sg.Text(key='vecino3_id_hoja', visible = False)),
             place(sg.Text(key='vecino3_distancia', visible = False)),
             place(sg.Text(key='vecino3_path', visible = False)),
-            place(sg.Input(key='vecino3_web_path', visible = False)),
-            place(sg.Button('Open', key='Open3', visible = False))
+            place(sg.Input(key='web_path_vecino3', visible = False)),
+            place(sg.Button('Open', key='Open 3', visible = False))
         ],
         [
             place(sg.Text(key='vecino4_id', visible = False)),
             place(sg.Text(key='vecino4_id_hoja', visible = False)),
             place(sg.Text(key='vecino4_distancia', visible = False)),
             place(sg.Text(key='vecino4_path', visible = False)),
-            place(sg.Input(key='vecino4_web_path', visible = False)),
-            place(sg.Button('Open', key='Open4', visible = False))
+            place(sg.Input(key='web_path_vecino4', visible = False)),
+            place(sg.Button('Open', key='Open 4', visible = False))
         ],
         [
             place(sg.Text(key='vecino5_id', visible=False)),
             place(sg.Text(key='vecino5_id_hoja', visible=False)),
             place(sg.Text(key='vecino5_distancia', visible=False)),
             place(sg.Text(key='vecino5_path', visible=False)),
-            place(sg.Input(key='vecino5_web_path', visible = False)),
-            place(sg.Button('Open', key='Open5', visible=False))   
+            place(sg.Input(key='web_path_vecino5', visible = False)),
+            place(sg.Button('Open', key='Open 5', visible=False))   
         ],
         [
             place(sg.Text(key='vecino6_id', visible = False)),
             place(sg.Text(key='vecino6_id_hoja', visible = False)),
             place(sg.Text(key='vecino6_distancia', visible = False)),
             place(sg.Text(key='vecino6_path', visible = False)),
-            place(sg.Input(key='vecino6_web_path', visible = False)),
-            place(sg.Button('Open', key='Open6', visible = False)) 
+            place(sg.Input(key='web_path_vecino6', visible = False)),
+            place(sg.Button('Open', key='Open 6', visible = False)) 
         ],
         [
             place(sg.Text(key='vecino7_id', visible = False)),
             place(sg.Text(key='vecino7_id_hoja', visible = False)),
             place(sg.Text(key='vecino7_distancia', visible = False)),
             place(sg.Text(key='vecino7_path', visible = False)),
-            place(sg.Input(key='vecino7_web_path', visible = False)),
-            place(sg.Button('Open', key='Open7', visible = False))
+            place(sg.Input(key='web_path_vecino7', visible = False)),
+            place(sg.Button('Open', key='Open 7', visible = False))
         ],
         [
             place(sg.Text(key='vecino8_id', visible = False)),
             place(sg.Text(key='vecino8_id_hoja', visible = False)),
             place(sg.Text(key='vecino8_distancia', visible = False)),
             place(sg.Text(key='vecino8_path', visible = False)),
-            place(sg.Input(key='vecino8_web_path', visible = False)),
-            place(sg.Button('Open', key='Open8', visible = False))
+            place(sg.Input(key='web_path_vecino8', visible = False)),
+            place(sg.Button('Open', key='Open 8', visible = False))
         ],
         [
             place(sg.Text(key='vecino9_id', visible = False)),
             place(sg.Text(key='vecino9_id_hoja', visible = False)),
             place(sg.Text(key='vecino9_distancia', visible = False)),
             place(sg.Text(key='vecino9_path', visible = False)),
-            place(sg.Input(key='vecino9_web_path', visible = False)),
-            place(sg.Button('Open', key='Open9', visible = False))
+            place(sg.Input(key='web_path_vecino9', visible = False)),
+            place(sg.Button('Open', key='Open 9', visible = False))
         ],
         [
             place(sg.Text(key='vecino10_id', visible = False)),
             place(sg.Text(key='vecino10_id_hoja', visible = False)),
             place(sg.Text(key='vecino10_distancia', visible = False)),
             place(sg.Text(key='vecino10_path', visible = False)),
-            place(sg.Input(key='vecino10_web_path', visible = False)),
-            place(sg.Button('Open', key='Open10', visible = False))  
+            place(sg.Input(key='web_path_vecino10', visible = False)),
+            place(sg.Button('Open', key='Open 10', visible = False))  
         ]
     ]
 
@@ -198,8 +205,8 @@ def main():
                 window[f'vecino{count}_id_hoja'].update(v['id_hoja'], visible = True)
                 window[f'vecino{count}_distancia'].update(v['distancia'], visible = True)
                 window[f'vecino{count}_path'].update(v['path'], visible = True) 
-                window[f'vecino{count}_web_path'].update(v['web_path'], visible = True) 
-                window[f'Open{count}'].update(visible=True)           
+                window[f'web_path_vecino{count}'].update(v['web_path'], visible = True) 
+                window[f'Open {count}'].update(visible=True)           
                 count+=1
             if (len(vecinos) < 10):
                 for x in range(len(vecinos)+1, 11):
@@ -207,8 +214,8 @@ def main():
                     window[f'vecino{x}_id_hoja'].update(visible = False)
                     window[f'vecino{x}_distancia'].update(visible = False)
                     window[f'vecino{x}_path'].update(visible = False) 
-                    window[f'vecino{x}_web_path'].update(visible = False) 
-                    window[f'Open{x}'].update(visible=False)  
+                    window[f'web_path_vecino{x}'].update(visible = False) 
+                    window[f'Open {x}'].update(visible=False)  
         elif event == 'OpenConsulta':
             filename = values['-IMAGEN-']
             if Path(filename).is_file():
@@ -217,87 +224,10 @@ def main():
                     img.show() 
                 except Exception as e:
                     print("Error: ", e)
-        elif event == 'Open1':
-            filename = values['vecino1_path']
-            if Path(filename).is_file():
-                try:
-                    img = Image.open(filename)
-                    img.show() 
-                except Exception as e:
-                    print("Error: ", e)
-        elif event == 'Open2':
-            filename = values['vecino2_path']
-            if Path(filename).is_file():
-                try:
-                    img = Image.open(filename)
-                    img.show() 
-                except Exception as e:
-                    print("Error: ", e)
-        elif event == 'Open3':
-            filename = values['vecino3_path']
-            if Path(filename).is_file():
-                try:
-                    img = Image.open(filename)
-                    img.show() 
-                except Exception as e:
-                    print("Error: ", e)
-        elif event == 'Open4':
-            filename = values['vecino4_path']
-            if Path(filename).is_file():
-                try:
-                    img = Image.open(filename)
-                    img.show() 
-                except Exception as e:
-                    print("Error: ", e)
-        elif event == 'Open5':
-            filename = values['vecino5_path']
-            if Path(filename).is_file():
-                try:
-                    img = Image.open(filename)
-                    img.show() 
-                except Exception as e:
-                    print("Error: ", e)
-        elif event == 'Open6':
-            filename = values['vecino6_path']
-            if Path(filename).is_file():
-                try:
-                    img = Image.open(filename)
-                    img.show() 
-                except Exception as e:
-                    print("Error: ", e)
-        elif event == 'Open7':
-            filename = values['vecino7_path']
-            if Path(filename).is_file():
-                try:
-                    img = Image.open(filename)
-                    img.show() 
-                except Exception as e:
-                    print("Error: ", e)
-        elif event == 'Open8':
-            filename = values['vecino8_path']
-            if Path(filename).is_file():
-                try:
-                    img = Image.open(filename)
-                    img.show() 
-                except Exception as e:
-                    print("Error: ", e)
-        elif event == 'Open9':
-            filename = values['vecino9_path']
-            if Path(filename).is_file():
-                try:
-                    img = Image.open(filename)
-                    img.show() 
-                except Exception as e:
-                    print("Error: ", e)
-        elif event == 'Open10':
-            filename = values['vecino10_path']
-            if Path(filename).is_file():
-                try:
-                    img = Image.open(filename)
-                    img.show() 
-                except Exception as e:
-                    print("Error: ", e)
-        
+        elif event.startswith('Open '):
+            vecino = event.split(' ')[1]
+            url = values[f'web_path_vecino{vecino}']
+            webbrowser.open(url)
     window.close()
 
 if __name__ == '__main__':
